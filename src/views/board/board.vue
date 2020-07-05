bef4a5dfb6ddf65c6e9e1ff67f7f5223c0909646
15515255978 <404174262@qq.com>
2020-07-05 17:32:51 +0800

    增加漂浮物彩蛋。

--- src/views/board/board.vue
+++ src/views/board/board.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="home">
-        <el-header class="header">
-            <div class="tool-container">
+        <header class="header">
+            <img class="mr30 hi "
+                 src="~src/assets/images/hi.png"
+                 width="50" height="50" />
+            <div class="left-tools dib">
                 <input class="filename-input" v-model="workbookName" />
-                <el-button type="primary" @click="onPickFile" size="mini"
-                    >导入数据</el-button
-                >
+                <el-button type="primary" @click="onPickFile" size="mini">导入数据</el-button>
                 <history @switch="switchWorkbook"></history>
                 <input
                     ref="fileInput"
@@ -14,21 +15,13 @@
                     accept=".xlsx,.csv,.xls"
                     @change="importFile"
                 />
-                <div class="right-tools">
-                    <img src="~src/assets/images/hi.png" width="50" height="50" class="mr30">
-                    <el-button type="success" size="mini" @click="exportFile"
-                        >导出数据</el-button
-                    >
-                    <el-button @click="exportFile" size="mini"
-                        >导出当前sheet</el-button
-                    >
-                </div>
             </div>
-        </el-header>
-        <c-table
-            class="c-table"
-            v-model="workbook.sheets[activeSheetName]"
-        ></c-table>
+            <div class="right-tools dib">
+                <el-button type="success" size="mini" @click="exportFile">导出数据</el-button>
+                <el-button @click="exportFile" size="mini">导出当前sheet</el-button>
+            </div>
+        </header>
+        <c-table class="c-table" v-model="workbook.sheets[activeSheetName]"></c-table>
         <sheetBar
             class="sheet-bar"
             :activeSheetName="activeSheetName"
